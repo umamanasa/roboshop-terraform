a909c9752b9c3207f064311fbc870c45856c03f3
umamanasa <umamanasa49@gmail.com>
2023-08-08 17:51:30 +0530

    creating instances

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ module "components" {
   source   = "git::https://github.com/umamanasa/tf-module-basic.git"
   for_each = var.components
 
-  zone_id           = var.zone_id
-  security_groups   = var.security_groups
-  name              = each.value["name"]
-  instance_type     = each.value["instance_type"]
+  zone_id             = var.zone_id
+  security_groups     = var.security_groups
+  name                = each.value["name"]
+  instance_type       = each.value["instance_type"]
 }
+
