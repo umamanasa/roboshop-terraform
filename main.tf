d5722f91d0075ecb780e5a3bb320b2d253d2eabe
umamanasa <umamanasa49@gmail.com>
2023-08-21 08:45:39 +0530

    creating load balancers

--- main.tf
+++ main.tf
@@ -9,11 +9,23 @@ module "vpc" {
   default_vpc_route_table_id    = var.default_vpc_route_table_id
   tags                          = var.tags
   env                           = var.env
-
 }
 
-output "vpc" {
-  value = module.vpc
+module "alb" {
+  source   = "git::https://github.com/umamanasa/tf-module-alb.git"
+
+  for_each                      = var.alb
+  internal                      = each.value["internal"]
+  lb_type                       = each.value["lb_type"]
+  sg_ingress_cidr               = each.value["sg_ingress_cidr"]
+  vpc_id                        = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+  subnets                       = each.value["internal"] ? local.app_subnets: data.aws_subnets.subnets.ids
+  tags                          = var.tags
+  env                           = var.env
+  sg_port                       = each.value["sg_port"]
 }
+
+
+
 
 
