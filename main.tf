c5788f028b0cbb9fc4a83c83d6d8937e83937a33
umamanasa <umamanasa49@gmail.com>
2023-08-22 17:29:26 +0530

    creating Db modules

--- main.tf
+++ main.tf
@@ -25,6 +25,22 @@ module "alb" {
   sg_port                       = each.value["sg_port"]
 }
 
+module "docdb" {
+  source   = "git::https://github.com/umamanasa/tf-module-docdb.git"
+
+  tags                          = var.tags
+  env                           = var.env
+  for_each                      = var.docdb
+  subnet_ids                    = local.db_subnets
+  backup_retention_period       = each.value["backup_retention_period"]
+  preferred_backup_window       = each.value["preferred_backup_window"]
+  skip_final_snapshot           = each.value["skip_final_snapshot"]
+  vpc_id                        = local.vpc_id
+  sg_ingress_cidr               = local.app_subnets_cidr
+  engine_version                = each.value["engine_version"]
+}
+
+
 
 
 
