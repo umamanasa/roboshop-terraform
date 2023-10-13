a320a38ffa18bc37fdb7aefa7169006ed13dec36
umamanasa <umamanasa49@gmail.com>
2023-10-13 18:59:46 +0530

    helm charts deployment-prod

--- main.tf
+++ main.tf
@@ -198,3 +198,53 @@ resource "aws_security_group_rule" "https-to-eks" {
   type              = "ingress"
   cidr_blocks       = var.ssh_ingress_cidr
 }
+
+resource "aws_iam_role" "eks-ssm" {
+  name = "${var.env}-eks-ssm-ro"
+#eks-ssm -pm-readonly roles trusted relationships
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "${module.eks.oidc_provider_arn}"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "${module.eks.oidc_provider}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+  inline_policy {
+    name = "${var.env}-eks-ssm-ro"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "kms:Decrypt",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "VisualEditor1",
+          "Effect" : "Allow",
+          "Action" : "ssm:DescribeParameters",
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+}
